ea4e69d74c99269a7504856446098433a33f33f7
jsd_fukui <fukui-takao@jsdnet.co.jp>
2017-08-15 14:27:19 +0900

    テストの追加

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe "#テスト実行" do
+    context "ArticleのFactoryを作成した場合" do
+      it "テストデータが作成される" do 
+        article_model = FactoryGirl.create(:article, text: "hogehoge")
+        ### テストが面にテストデータを表示する
+        p article_model
+      end
+    end
+  end
 end
