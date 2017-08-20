cf9899be575cb4ca26b896dcaea5bf1088d6de34
ftakao2007 <ftakao2007@gmail.com>
2017-08-20 16:50:58 +0900

    コメントを記事作成時に同時に書き込むようにモデルを修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,27 @@
 class ArticlesController < ApplicationController
   def new
     @article = Article.new
+    @comment = Comment.new
   end
 
   def create
     #@article = Article.new(params[:article]).permit(:title, :text)
     @article = Article.new(article_params)
+    @comment = Comment.new(comment_params)
 
-    if @article.save
+    Article.transaction do
+      @comment.article = @article
+      @article.save!
+      @comment.save!
+      flash[:notice] = "記事を投稿しました。"
       redirect_to @article
-    else
-      render 'new'
     end
-
-    #render plain: params[:article].inspect
-    #render html: params[:article].inspect
   end
 
   def show
     @article = Article.find(params[:id])
+    #@comment = Comment.find_by(article_id: params[:id])
+    @comment = @article.comment
   end
 
   def index
@@ -50,5 +53,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :text)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
   end
 end
